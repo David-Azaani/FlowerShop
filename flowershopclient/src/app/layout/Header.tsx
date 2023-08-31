eca78ebba646ff1cf43f1a1ba62dcd23b252075d
f00000b <f00000b@gmail.com>
2023-09-01 01:49:43 +0330

    🐌Theme

--- flowershopclient/src/app/layout/Header.tsx
+++ flowershopclient/src/app/layout/Header.tsx
@@ -1,10 +1,14 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-
-export default function Header() {
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+interface Props {
+  changeTheme: () => void;
+  darkMode: boolean;
+}
+export default function Header({ darkMode, changeTheme }: Props) {
   return (
-    <AppBar position="static" sx={{mb:4}}>
+    <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar>
         <Typography variant="h6">FlowerShop</Typography>
+        <Switch checked={darkMode} onChange={changeTheme} />
       </Toolbar>
     </AppBar>
   );
