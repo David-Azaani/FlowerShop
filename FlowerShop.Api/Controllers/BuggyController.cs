2e3650c0d1f088305c5e3f3a09e3f7ca9b3e73a4
f00000b <f00000b@gmail.com>
2023-09-02 22:56:59 +0330

    🐌AddBuggyController

---
+++ FlowerShop.Api/Controllers/BuggyController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShop.Api.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        public BuggyController(ILogger<BaseApiController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound() => NotFound();
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest() => BadRequest("This is a Bad Request");
+        [HttpGet("un-authorized")]
+        public ActionResult GetUnauthorized() => Unauthorized();
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "This is the first error");        
+            ModelState.AddModelError("Problem2", "This is the second error");
+            return ValidationProblem();
+
+         }
+        [HttpGet("server-error")]
+        public ActionResult GetServerError() {
+
+            throw new Exception("This is a server Error");
+
+        }
+
+    }
+}
