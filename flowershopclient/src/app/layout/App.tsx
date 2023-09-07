a04982f930558cc4a86c93fd8c2a0adeb9660403
f00000b <f00000b@gmail.com>
2023-09-07 12:17:07 +0330

    🐌Impelimenting and using context Provier

--- flowershopclient/src/app/layout/App.tsx
+++ flowershopclient/src/app/layout/App.tsx
@@ -5,12 +5,29 @@ import {
   createTheme,
 } from "@mui/material";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useStoreConetxt } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const { setBasket } = useStoreConetxt();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie("buyerId");
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -24,6 +41,10 @@ function App() {
   function setDarkModeTheme() {
     setDarkMode(!darkMode);
   }
+
+  if (loading)
+    return <LoadingComponent loadingMessage="Initializing app ..." />;
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
